5830fa1640bd5837ab59a8d188771d119517423c
Rylie Castell <rylie.castell@gmail.com>
2025-03-18 12:36:33 +0100

    add training methods

--- Box Simulation/Boxer.swift
+++ Box Simulation/Boxer.swift
@@ -14,7 +14,11 @@ struct Boxer {
     let nationality: String
     var resiliencePoints: Int
     var powerPoints: Int
-    var endurance: Int
+    var endurance: Int {
+        didSet {
+            print("Endurance changed from \(oldValue) to \(endurance).")
+        }
+    }
     var hitRate: Int
     
     func description() {
@@ -23,5 +27,16 @@ struct Boxer {
     
     mutating func birthday() {
         age += 1
+    }
+    
+    mutating func training() {
+        if endurance >= 10 {
+            powerPoints += 10
+            endurance -= 10
+        }
+    }
+    
+    mutating func regeneration() {
+        endurance += 15
     }
 }

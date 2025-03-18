7be57976bf281d9dffdab2cb4c7bb83a674f9ac4
Rylie Castell <rylie.castell@gmail.com>
2025-03-18 12:29:31 +0100

    add boxingring struct

---
+++ Box Simulation/BoxingRing.swift
@@ -0,0 +1,15 @@
+//
+//  BoxingRing.swift
+//  Box Simulation
+//
+//  Created by Rylie Castell on 18.03.25.
+//
+
+import Foundation
+
+struct BoxingRing {
+    var name: String
+    var country: String
+    var numberViewers: Int
+    var isOutdoor: Bool
+}

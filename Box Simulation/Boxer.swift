29113831427ce96b92aecbf86301792dc4e6530e
Rylie Castell <rylie.castell@gmail.com>
2025-03-18 12:27:27 +0100

    add boxer struct

---
+++ Box Simulation/Boxer.swift
@@ -0,0 +1,19 @@
+//
+//  Boxer.swift
+//  Box Simulation
+//
+//  Created by Rylie Castell on 17.03.25.
+//
+
+import Foundation
+
+struct Boxer {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    let nationality: String
+    var resiliencePoints: Int
+    var powerPoints: Int
+    var endurance: Int
+    var hitRate: Int
+}
